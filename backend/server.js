e5764ce17aa56fa80a867528d07c7db2252b6187
VasantA2730 <vasantarul12@gmail.com>
2025-11-22 16:54:27 -0600

    Basic server side code to connect to DB. Not quite working yet, just pushing to show structure

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,24 @@
 require('dotenv').config
 const express = require('express');
+const { sql } = require("./db.js");
 
 const app = express();
 
-const port = process.env.PORT;
+const port = process.env.SERVER_PORT;
+
+app.get("/", (req, res) => {
+    const getBuildings = async () => {
+        const buildings = sql`SELECT * FROM buildings`;
+
+        console.log(buildings);
+
+        return buildings;
+    };
+
+    getBuildings();
+    
+    res.end();
+});
 
 app.listen(port, () => {
     console.log(`Listening on Port ${port}`);
