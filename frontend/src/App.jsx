0688f229a55e8781c1c7e9e7f2bc486f8f71f4c0
semzit <zitouni.skz@gmail.com>
2025-11-22 16:13:09 -0600

    added ws send

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { Button, Card } from 'react-bootstrap'
+import { useWebSocket } from './hooks/webHooks'
 import './App.css'
 import { HashRouter, Route, Routes } from 'react-router'
 import Screen from './components/Screen'
@@ -7,9 +8,22 @@ import Screen from './components/Screen'
 function App() {
   const [count, setCount] = useState(0);
 
+  const { isConnected, message, send } = useWebSocket("ws://localhost:8080/ws");
+
+  
+
+  {/* code for sending update*/}
+
+  <Button onClick={() => {
+    send({ type: "paint", x: document.getElementById("xCoord").value, y: document.getElementById("yCoord").value , color : document.getElementById("color").value});
+  }}>
+    Send Update
+  </Button>
+
   return <Card>
 
     <Screen/>
+
 
   </Card>
 }
