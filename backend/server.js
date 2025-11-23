6365f16168c582a80c5110acc41f9d7e6206648c
VasantA2730 <vasantarul12@gmail.com>
2025-11-23 00:02:29 -0600

    Fixed some import statements

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import { getState } from "./services/database.js"
 require('dotenv').config();
 const express = require('express');
 
@@ -11,6 +12,10 @@ const authRoutes = require("./services/authService.js");
 app.use(express.json());
 
 app.use('/auth', authRoutes);
+
+app.get("/", (req, res) => {
+    getState();
+});
 
 app.listen(port, () => {
     console.log(`Listening on Port ${port}`);
--- backend/services/database.js
+++ backend/services/database.js
@@ -1,12 +1,24 @@
-import {getRedis, loadRedis} from './redisService.js';
-const { pool } = ;
+import { getRedis, loadRedis } from './redisService.js';
+const { pool } = require("../db.js");
 
 const getState = () => {
     // Load all buildingIds from SQL
+    const query = `SELECT b.id FROM buildings b`;
 
-
+    pool.query(query, (err, qres) => {
+        if (err) {
+            res.status(500).json({
+                message: "Query failed"
+            });
+        } else {
+            const obj = qres.rows;
+            console.log(obj);
+        }
+    });
 
     // for each buildingId, call loadRedis
+
+    
 
     loadRedis(buildingId, state);
 }; 
